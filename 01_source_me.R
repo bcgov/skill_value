d87eae7856e310b02aea284d6722f4588cced7ca
Rich-P-Martin <richard.martin@gov.bc.ca>
2023-08-08 21:07:24 -0700

    use the raw skill data that has both level and importance to create geometric mean.

--- 01_source_me.R
+++ 01_source_me.R
@@ -3,51 +3,64 @@ library(tidyverse)
 library(here)
 library(janitor)
 library(broom)
+library(readxl)
 #functions---------------
 av_wrapper <- function(term){
   car::avPlot(mod, variable=term)
   recordPlot()
 }
-#read in the data--------------------
+
+# the program------------------------
+mapping <- read_excel(here("data","onet2019_soc2018_noc2016_noc2021_crosswalk.xlsx"))%>%
+  mutate(noc2021=str_pad(noc2021, "left", pad="0", width=5))%>%
+  unite(noc, noc2021, noc2021_title, sep=": ")%>%
+  select(noc, o_net_soc_code = onetsoc2019)%>%
+  distinct()
+
+skills_raw <- read_excel(here("data", "Skills.xlsx"))%>%
+    clean_names()%>%
+    select(o_net_soc_code, element_name, scale_name, data_value)%>%
+    pivot_wider(names_from = scale_name, values_from = data_value)%>%
+    mutate(score=log(sqrt(Importance*Level)+1))%>%
+    select(-Importance, -Level)%>%
+    inner_join(mapping)%>%
+    select(-o_net_soc_code)%>%
+    select(noc, everything())%>%
+    group_by(noc, element_name)%>%
+    summarise(across(where(is.numeric), \(x) mean(x, na.rm = TRUE)))%>% #mapping from SOC to NOC is not one to one: mean give one value per NOC
+    pivot_wider(names_from = element_name, values_from = score)%>%
+    separate(noc, into=c("noc2021", "noc2021_title"), sep=": ")
+
 income <- readxl::read_excel(here("data",
                                   "Employment income by NOC5_2021 census.xlsx"),
                              skip = 3,
                              na = "x")%>%
   select(noc="...1", income = `Median employment income ($)`)%>%
-  mutate(log_med_income=log(income), .keep="unused")%>%
+  mutate(Income = log(income), .keep="unused")%>%
   separate(noc, into=c("noc2021", "noc2021_title"), sep = 6)%>%
   mutate(noc2021=trimws(noc2021))
 
-skills_raw <- readxl::read_excel(here::here("data","skills_original.xlsx"),
-                         col_types = c("text","text","text","numeric"))%>%
-  mutate(noc2021=str_pad(noc2021, width=5, side="left", pad="0"))%>%
-  pivot_wider(id_cols=contains("noc"), names_from = contains("name"), values_from = contains("value"))%>%
-  clean_names()%>%
-  mutate(across(where(is.numeric), log))
 
 #join the income and skill data-----------------------
 tbbl <- inner_join(income, skills_raw)%>%
   unite("noc", noc2021, noc2021_title, sep=": ")%>%
   column_to_rownames("noc")
 #fit a linear model to log transformed data-------------------------
-mod <- lm(log_med_income ~ ., data=tbbl)
+mod <- lm(Income ~ ., data=tbbl)
 sorted_estimates <- mod%>%
   tidy()%>%
   filter(term!="(Intercept)")%>%
   arrange(desc(estimate))
 #plot all the elasticities---------------------------
-elasticity_plt <- sorted_estimates%>%
-  mutate(term=str_to_title(str_replace_all(term,"_"," ")),
-         estimate=estimate/100
-         )%>%
-ggplot(aes(estimate, fct_reorder(term, estimate)))+
-  geom_col()+
-  scale_x_continuous(labels = scales::percent)+
-  labs(x="Income Elasticity with respect to skill",
-       y="",
-       title="Relationship between Skills and Income across Occupations",
-       subtitle="holding all other skills constant")+
-  theme_minimal()
+(elasticity_plt <- sorted_estimates%>%
+   mutate(term=str_replace_all(term,"`",""))%>%
+  ggplot(aes(estimate, fct_reorder(term, estimate)))+
+    geom_col()+
+    labs(x="Income Elasticity with respect to skill",
+         y="",
+         title="Relationship between Skills and Income across Occupations",
+         subtitle="holding all other skills constant")+
+    theme_minimal())
 
 sorted_estimates <- sorted_estimates%>%
   mutate(av_plot=map(term, av_wrapper))
